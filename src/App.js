a08b4b3fb15d47839e8eaf2cf165173042689f98
Akhilesh Raturi <44131270+akhileshRaturi@users.noreply.github.com>
2021-11-13 23:28:44 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="content">
+        <h1>App component is Here</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
